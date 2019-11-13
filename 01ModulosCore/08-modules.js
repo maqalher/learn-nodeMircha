7ae00f211e243b302ac33feee569415e7c05f327
maqalher <maqalher@hotmail.com>
2019-11-13 00:55:03 -0600

    clock modules

--- 01ModulosCore/08-modules.js
+++ 01ModulosCore/08-modules.js
@@ -1,10 +1,16 @@
 'use strict';
 
 //var myData = require('./my-data.js');
-var myData = require('./my-data');
+var myData = require('./my-data'),
+    Clock = require('./clock-es5'),
+    cucu = new Clock();
 
 console.log(
     myData.name,
     myData.email,
     myData._phone
 );
+
+cucu.on('tictac', function(){
+    cucu.theTime();
+});
