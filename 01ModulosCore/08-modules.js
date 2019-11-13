82807236cd5f586ceb680f7b5dc1e2ad773a8fea
maqalher <maqalher@hotmail.com>
2019-11-13 00:08:02 -0600

    modules

---
+++ 01ModulosCore/08-modules.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//var myData = require('./my-data.js');
+var myData = require('./my-data');
+
+console.log(
+    myData.name,
+    myData.email,
+    myData._phone
+);
---
+++ 01ModulosCore/my-data.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var name = 'Alan',
+    email = 'maqalher@hotmail.com',
+    _phone = '01800039393933';
+
+module.exports.name = name;
+module.exports.email = email;
